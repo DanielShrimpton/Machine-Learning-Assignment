b9e06834f71c71e44dece21e762d1a362116bae7
Daniel Shrimpton <44175578+DanielShrimpton@users.noreply.github.com>
2020-02-23 13:03:48 +0000

    Categorical data -> numerical data, some predictions run on it

--- classifier.py
+++ classifier.py
@@ -3,8 +3,11 @@ import pandas as pd
 from pandas.plotting import scatter_matrix
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+from sklearn.tree import DecisionTreeRegressor
 from mpl_toolkits.mplot3d import Axes3D
 
 FOLDER = './data/'  # folder that contains the csv data files
@@ -21,7 +24,7 @@ def first_attempt():
     scatter_matrix(studentInfo.train_set.dataset)
     plt.show()
 
-    studentInfo.train_set.dataset.plot(kind='scatter', x='studied_credits', y='result', alpha=0.1)
+    studentInfo.train_set.dataset.plot(kind='scatter', x='studied_creduts', y='result', alpha=0.1)
     plt.show()
 
     # studentInfo2 = studentInfo.drop('result', 1)
@@ -68,7 +71,7 @@ def first_attempt():
     big2.drop("region", 1, inplace=True)
     # big2.drop("final_result", 1, inplace=True)
     big2.info()
-    big2_cat = big2.drop("studied_credits", 1)
+    big2_cat = big2.drop("studied_creduts", 1)
     big2_cat.drop("score", 1, inplace=True)
     big2_cat.drop("result", 1, inplace=True)
     big2_cat.drop("num_of_prev_attempts", 1, inplace=True)
@@ -91,6 +94,93 @@ def first_attempt():
 
 
 student_assessment = ProcessData(folder=FOLDER, filename="studentAssessment.csv")
-student_assessment.info()
+# student_assessment.info()
 assessments = ProcessData(folder=FOLDER, filename="assessments.csv")
-assessments.info()
+# assessments.info()
+
+test = student_assessment.dataset.merge(assessments.dataset, how='left')
+student_info = ProcessData(folder=FOLDER, filename='studentInfo.csv')
+test = ProcessData(data=[test.merge(student_info.dataset, how='left')], read=False)
+
+test = ProcessData(read=False, data=[test.dataset.dropna(axis=0)])
+test.drop('date')
+test.drop('date_submitted')
+test.drop('is_banked')
+test.label_encode(15, 'result')
+length = len(test.dataset.final_result)
+for i in range(length):
+    if test.dataset.final_result.iat[i] == 'Withdrawn':
+        test.dataset.result.iat[i] = 0
+    elif test.dataset.final_result.iat[i] == 'Fail':
+        test.dataset.result.iat[i] = 1
+    elif test.dataset.final_result.iat[i] == 'Pass':
+        test.dataset.result.iat[i] = 2
+    elif test.dataset.final_result.iat[i] == 'Distinction':
+        test.dataset.result.iat[i] = 3
+test.drop('final_result')
+test.drop('code_module')
+test.drop('code_presentation')
+test.drop('region')
+test.label_encode(7, 'imd_band_')
+test.drop('imd_band')
+test.label_encode(7, 'age_band_')
+test.drop('age_band')
+test.label_encode(9, 'disability_')
+test.drop('disability')
+test.label_encode(5, 'gender_')
+test.drop('gender')
+test.drop('assessment_type')
+test.label_encode(4, 'highest_education_')
+for i in range(length):
+    if test.dataset.highest_education_.iat[i] == 0:  # If it is A Level
+        test.dataset.highest_education_.iat[i] = 2
+    if test.dataset.highest_education_.iat[i] == 1:  # If it is HE
+        test.dataset.highest_education_.iat[i] = 3
+    if test.dataset.highest_education_.iat[i] == 2:  # If it is Lower than A Level
+        test.dataset.highest_education_.iat[i] = 1
+    if test.dataset.highest_education_.iat[i] == 3:  # If it is No Formal quals
+        test.dataset.highest_education_.iat[i] = 0
+print(np.array(['No Formal quals', 'Lower Than A Level', 'A Level or Equivalent', 'HE Qualification',
+                'Post Graduate Qualification']))
+test.drop('highest_education')
+# test.info()
+test.test_train_split()
+test_labels = ProcessData(read=False, tt=True, data=[test.dataset['result'], test.train_set.dataset['result'],
+                                                     test.test_set.dataset['result']])
+test.drop('result')
+
+lin_reg = LinearRegression()
+lin_reg.fit(test.train_set.dataset, test_labels.train_set.dataset)
+some_data = test.train_set.dataset.iloc[:5]
+print("Predictions:", lin_reg.predict(some_data))
+some_labels = test_labels.train_set.dataset.iloc[:5]
+print("Labels:", list(some_labels))
+
+test_predictions = lin_reg.predict(test.train_set.dataset)
+lin_mse = mean_squared_error(test_labels.train_set.dataset, test_predictions)
+lin_rmse = np.sqrt(lin_mse)
+print(lin_rmse)
+
+tree_reg = DecisionTreeRegressor()
+tree_reg.fit(test.train_set.dataset, test_labels.train_set.dataset)
+test_predictions = tree_reg.predict(test.train_set.dataset)
+tree_mse = mean_squared_error(test_labels.train_set.dataset, test_predictions)
+tree_rmse = np.sqrt(tree_mse)
+print(tree_rmse)  # OVER FITTED!!!
+scores = cross_val_score(tree_reg, test.train_set.dataset, test_labels.train_set.dataset,
+                         scoring="neg_mean_squared_error", cv=10)
+tree_rmse_scores = np.sqrt(-scores)
+
+
+def display_scores(scores):
+    print("Scores:", scores)
+    print("Mean:", scores.mean())
+    print("Standard Deviation:", scores.std())
+
+
+display_scores(tree_rmse_scores)
+
+lin_scores = cross_val_score(lin_reg, test.train_set.dataset, test_labels.train_set.dataset,
+                             scoring='neg_mean_squared_error', cv=10)
+lin_rmse_scores = np.sqrt(-lin_scores)
+display_scores(lin_rmse_scores)
